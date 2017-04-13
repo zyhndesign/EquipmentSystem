7f3eb0727a1a278e02ad8b3d8e1ee84d0feeeb60
codeliling <maximuslee@126.com>
2017-04-13 15:40:36 +0800

    fix bug

--- src/main/java/com/cidic/equipment/controller/CategoryController.java
+++ src/main/java/com/cidic/equipment/controller/CategoryController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cidic.equipment.exception.EquipmentException;
-import com.cidic.equipment.model.Brand;
 import com.cidic.equipment.model.Category;
 import com.cidic.equipment.model.CategoryTableModel;
 import com.cidic.equipment.model.ListResultModel;
